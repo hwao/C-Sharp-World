170ea5cf60df24b09d98c31b629b1eec52277244
hwao <halicki@gmail.com>
2013-01-30 21:13:10 +0100

    World / Map / Hill - add - Dodalem textury gorek

---
+++ World/World/Map/Field/Hill/HillSE.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace World.Map.Field.Hill
+{
+    class HillSE : World.Map.Field.iField
+    {
+        public HillSE(Game1 Game)
+            : base(Game)
+        {
+
+        }
+
+        public override void LoadContent(ContentManager Content)
+        {
+            this.mSpriteTexture = Content.Load<Texture2D>("Field/Hill/hillSE");
+        }
+
+        public override string ToString()
+        {
+            return "HSE";
+        }
+    }
+}
+
