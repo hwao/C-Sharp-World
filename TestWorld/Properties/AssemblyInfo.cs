732a50a7bb71fb0c2eaf66b5f9b0cfbaac6cddeb
hwao <halicki@gmail.com>
2013-02-02 01:43:45 +0100

    TestWorld - add - Utworzenie pierwszych testow do projektu

--- TestWorld/Properties/AssemblyInfo.cs
+++ TestWorld/Properties/AssemblyInfo.cs
@@ -5,24 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("World")]
-[assembly: AssemblyProduct("World")]
+[assembly: AssemblyTitle("TestWorld")]
 [assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestWorld")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type. Only Windows
-// assemblies support COM.
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// On Windows, the following GUID is for the ID of the typelib if this
-// project is exposed to COM. On other platforms, it unique identifies the
-// title storage container when deploying this assembly to the device.
-[assembly: Guid("3f14c622-7336-4c93-95be-9c2ff1f684d6")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("74bdc383-6380-489a-b7d4-40c0f0cbf9bb")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,6 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
-
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("TestWorld")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestWorld/WorldMapTest.cs
@@ -0,0 +1,97 @@
+﻿using World;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using World.Map.Field;
+
+namespace TestWorld
+{
+    
+    
+    /// <summary>
+    ///This is a test class for WorldMapTest and is intended
+    ///to contain all WorldMapTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class WorldMapTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for getField
+        ///</summary>
+        [TestMethod()]
+        public void getFieldTest()
+        {
+            int width = 2; 
+            int height = 2;
+            Game1 Game = new Game1();
+            WorldMap target = new WorldMap(width, height, Game); // TODO: Initialize to an appropriate value
+
+            iField actual;
+
+            // Element istniejacy
+            actual = target.getField(0,0);
+            Assert.AreEqual(0, actual.X);
+            Assert.AreEqual(0, actual.Y);
+
+            // Element 'poza mapa' - gorna granica
+            actual = target.getField(width+2,height+2);
+            Assert.AreEqual(width-1, actual.X);
+            Assert.AreEqual(height-1, actual.Y);
+
+            // Element 'poza mapa' - dolna granica
+            actual = target.getField(-1, -1);
+            Assert.AreEqual(0, actual.X);
+            Assert.AreEqual(0, actual.Y);
+        }
+    }
+}
