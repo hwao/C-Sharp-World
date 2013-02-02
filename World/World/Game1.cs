679bc06dae903484d5a51a8204e6ca24aa795429
hwao <halicki@gmail.com>
2013-02-02 01:53:45 +0100

    World/Game1.cs - refactoring - sprzatam w kodzie, wywalam nieuzywane fragmenty, podpinam klase licznika fps zamiast logiki w game - add - dodaje widocznosc kursora myszki - add - dodaje przechytywanie pozycji myszki i liczenia na tej podstawie jaki element w viewport znajduje sie pod myszka

--- World/World/Game1.cs
+++ World/World/Game1.cs
@@ -16,39 +16,43 @@ namespace World
     /// </summary>
     public class Game1 : Microsoft.Xna.Framework.Game
     {
-        // FPS COUNTER
+        /// <summary>
+        /// Sta³a okreœlaj¹ca czy kod jest stabilny (produkcyjny)
+        /// czy te¿ jest to wersja developerska - posiada mniej fixów w kodzie, ulatwia znajdowanie b³êdów
+        /// </summary>
+        public const bool STABLE = false;
+        bool debugViewport = false;
+
         public SpriteFont _spr_font;
-        int _total_frames = 0;
-        float _elapsed_time = 0.0f;
-        int _fps = 0;
-        // END FPS COUNTER
 
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        Vector2 v2Camera = new Vector2(0, 0);
-
+        private Element.DisplayFps DisplayFps;
         Viewport[] Viewport;
         Player Player;
 
         WorldMap WorldMap;
-       // Rectangle ViewportOld;
         Texture2D pixel;
 
         KeyboardState LastkeyState;
 
-        bool debugViewport = false;
+        World.Map.Field.iField OldField;
 
+        /// <summary>
+        /// Start gry
+        /// </summary>
         public Game1()
         {
             this.IsFixedTimeStep = false;
+            this.IsMouseVisible = true;
 
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
 
             graphics.PreferredBackBufferWidth = 1820;
             graphics.PreferredBackBufferHeight = 1100;
-            if (this.debugViewport!=true)
+            if (this.debugViewport != true)
             {
                 graphics.PreferredBackBufferWidth /= 2;
                 graphics.PreferredBackBufferHeight /= 2;
@@ -69,6 +73,8 @@ namespace World
         /// </summary>
         protected override void Initialize()
         {
+            this.DisplayFps = new Element.DisplayFps();
+
             int size = 100;
             this.WorldMap = new WorldMap(size, size, this);
 
@@ -77,7 +83,7 @@ namespace World
             Rectangle Screen = new Rectangle(0, 0, 400, 300);
             Screen.Width = graphics.PreferredBackBufferWidth;
             Screen.Height = graphics.PreferredBackBufferHeight;
-            if (this.debugViewport==true)
+            if (this.debugViewport == true)
             {
                 Screen.X = 200;
                 Screen.Y = 200;
@@ -86,6 +92,7 @@ namespace World
             }
             this.Viewport[0] = new Viewport(this.WorldMap, Screen, new Vector2(100, 50), new Vector2(0, 0));
             this.LastkeyState = Keyboard.GetState();
+            this.OldField = this.WorldMap.getField(0, 0);
 
             this.Player = new Player(new Vector2(100, 100), 10);
 
@@ -97,7 +104,7 @@ namespace World
 
         public void UpdateTitle()
         {
-            Window.Title = string.Format("Mapa: {0} x {1} Okno: {2} x {3} Zoom: {4}", this.WorldMap.getWidth(), this.WorldMap.getHeight(), graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight, Viewport[0].Zoom );
+            Window.Title = string.Format("Mapa: {0} x {1} Okno: {2} x {3} Zoom: {4}", this.WorldMap.getWidth(), this.WorldMap.getHeight(), graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight, Viewport[0].Zoom);
         }
 
         /// <summary>
@@ -109,7 +116,6 @@ namespace World
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            _spr_font = Content.Load<SpriteFont>("SpriteFont1");
 
             pixel = new Texture2D(GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
             pixel.SetData(new[] { Color.White }); // so that we can draw whatever color we want on top of it
@@ -118,6 +124,9 @@ namespace World
             this.WorldMap.LoadContent(this.Content);
 
             this.Player.LoadContent(this.Content);
+
+            _spr_font = Content.Load<SpriteFont>("SpriteFont1");
+            this.DisplayFps.LoadContent(_spr_font);
         }
 
         /// <summary>
@@ -136,15 +145,15 @@ namespace World
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            // Update
-            _elapsed_time += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
- 
-            // 1 Second has passed
-            if (_elapsed_time >= 1000.0f)
+            this.DisplayFps.Update(gameTime);
+
+            MouseState mouseState = Mouse.GetState();
+            World.Map.Field.iField Field = this.Viewport[0].getField(mouseState.X, mouseState.Y);
+            if (Field != OldField)
             {
-                _fps = _total_frames;
-                _total_frames = 0;
-                _elapsed_time = 0;
+                Field.alpha = 0.5f;
+                OldField.alpha = 1.0f;
+                OldField = Field;
             }
 
             KeyboardState keyState = Keyboard.GetState();
@@ -171,17 +180,17 @@ namespace World
 
 
             if (keyState.IsKeyDown(Keys.Left))
-                this.Player.Position.X-=2;
+                this.Player.Position.X -= 2;
             if (keyState.IsKeyDown(Keys.Right))
-                this.Player.Position.X+=2;
+                this.Player.Position.X += 2;
             if (keyState.IsKeyDown(Keys.Up))
-                this.Player.Position.Y-=2;
+                this.Player.Position.Y -= 2;
             if (keyState.IsKeyDown(Keys.Down))
-                this.Player.Position.Y+=2;
-            
+                this.Player.Position.Y += 2;
+
 
             // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || keyState.IsKeyDown(Keys.Q) || keyState.IsKeyDown(Keys.Escape) )
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || keyState.IsKeyDown(Keys.Q) || keyState.IsKeyDown(Keys.Escape))
                 this.Exit();
 
             this.LastkeyState = keyState;
@@ -193,37 +202,36 @@ namespace World
             base.Update(gameTime);
         }
 
+        
+
         /// <summary>
         /// This is called when the game should draw itself.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            // Only update total frames when drawing
-            _total_frames++;
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
             spriteBatch.Begin();
             //this.WorldMap.Draw(spriteBatch, v2Camera, ViewportOld );
 
-            
 
-            foreach (Viewport View in this.Viewport ) {
+
+            foreach (Viewport View in this.Viewport)
+            {
                 View.Draw(spriteBatch);
-                if (this.debugViewport == true) 
+                if (this.debugViewport == true)
                     DrawBorder(View.Screen, 2, Color.Red);
             }
 
 
-            this.Player.Draw(spriteBatch, this.Viewport[0].Camera, this.Viewport[0].Screen, 1.0f );
+            this.Player.Draw(spriteBatch, this.Viewport[0].Camera, this.Viewport[0].Screen, 1.0f);
             //DrawBorder(this.Player.getRectangle( this.Viewport[0].Camera, this.Viewport[0].Screen ), 1, Color.Red);
 
-            spriteBatch.DrawString(_spr_font, string.Format("FPS={0}", _fps),
-                new Vector2(10.0f, 20.0f), Color.Red);
-         
-            spriteBatch.End();
+            this.DisplayFps.Draw(gameTime, spriteBatch);
 
+            spriteBatch.End();
             base.Draw(gameTime);
         }
 
