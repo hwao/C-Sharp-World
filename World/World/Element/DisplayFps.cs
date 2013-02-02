b261f48c86974277f04e551403f2f4781f2e791c
hwao <halicki@gmail.com>
2013-02-02 01:42:00 +0100

    World / Element / DisplayFps.cs - add - Obiekt odpowiedzialny za obliczanie i wyswietlanie ilosci FPS w grze

---
+++ World/World/Element/DisplayFps.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace World.Element
+{
+    class DisplayFps
+    {
+        public SpriteFont _spr_font;
+        int _total_frames = 0;
+        float _elapsed_time = 0.0f;
+        int _fps = 0;
+
+        public void LoadContent(SpriteFont _spr_font)
+        {
+            this._spr_font = _spr_font;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            // Update
+            _elapsed_time += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            // 1 Second has passed
+            if (_elapsed_time >= 1000.0f)
+            {
+                _fps = _total_frames;
+                _total_frames = 0;
+                _elapsed_time = 0;
+            }
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch) 
+        {
+            // Only update total frames when drawing
+            _total_frames++;
+
+            spriteBatch.DrawString(_spr_font, string.Format("FPS={0}", _fps),
+                new Vector2(10.0f, 20.0f), Color.Red);
+        }
+    }
+}
