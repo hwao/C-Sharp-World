df719984d1cd1e021df073c9bc7d24fe9715ec3c
hwao <halicki@gmail.com>
2013-02-02 01:46:21 +0100

    World / WorldMap.cs - refactor - optymalizacja kodu, pozmienialem nieoptymalne obliczenia

--- World/World/WorldMap.cs
+++ World/World/WorldMap.cs
@@ -39,14 +39,15 @@ namespace World
             {
                 for (int h = 0; h < this.height; h++)
                 {
-                    Vector2 pos = new Vector2(0,0);
+
+                    Vector2 pos = new Vector2(0, 0);
 
                     pos.Y = w * (64 / 2) - (w * 7);
                     pos.X = -w * (100 / 2); // -(w);
 
                     pos.Y += h * ((100 / 2) / 2);
-                    pos.X += h * (100/2);
-
+                    pos.X += h * (100 / 2);
+                    /*
                     if (h % 5 == 1 && w % 5 == 1)
                     {
                         this.fieldList[w, h] = new World.Map.Field.Grass(this.Game);
@@ -89,27 +90,22 @@ namespace World
                     {
                         this.fieldList[w, h] = new World.Map.Field.Grass(this.Game);
                     }
+                     * */
+                    this.fieldList[w, h] = new World.Map.Field.Grass(this.Game);
 
-                    
-                    
+
                     this.fieldList[w, h].mPosition = pos;
                     this.fieldList[w, h].X = w;
                     this.fieldList[w, h].Y = h;
-
-                    if (w == 0 && h == 0)
-                    {
-                        this.mapZero.X = pos.X;
-                        this.mapZero.Y = pos.Y;
-                        Console.WriteLine("TEST: {0} {1}", this.mapZero.X, this.mapZero.Y);
-                    }
                 }
             }
+            this.mapZero = this.fieldList[0,0].mPosition;
 
             for (int w = 0; w < this.width; w++)
             {
                 for (int h = 0; h < this.height; h++)
                 {
-                    this.fieldList[w, h].bindAdjacentField( this );
+                    this.fieldList[w, h].bindAdjacentField(this);
                 }
             }
 
@@ -136,15 +132,15 @@ namespace World
 
                 k = steps - step - 1;
 
-                this.yKubelek[step] = new World.Map.Field.iField[step+1];
-                this.yKubelek[k] = new World.Map.Field.iField[step+1];
+                this.yKubelek[step] = new World.Map.Field.iField[step + 1];
+                this.yKubelek[k] = new World.Map.Field.iField[step + 1];
 
                 for (f = 0; f <= step; f++)
                 {
-                    this.yKubelek[step][f] = this.fieldList[x,y];
+                    this.yKubelek[step][f] = this.fieldList[x, y];
                     if (step != half)
                     {
-                       this.yKubelek[k][step-f] = this.fieldList[half-x,half-y];
+                        this.yKubelek[k][step - f] = this.fieldList[half - x, half - y];
                     }
                     x++;
                     y--;
@@ -152,20 +148,35 @@ namespace World
             }
         }
 
-
-        public World.Map.Field.iField getField( int X, int Y )
+        /// <summary>
+        /// Jak generujemy mape to kazde pole szuka sobie sasiada, dla pola polozonego na skraju musi znajowac odpowiedniego sasiada
+        /// </summary>
+        /// <param name="X"></param>
+        /// <param name="Y"></param>
+        /// <returns></returns>
+        public World.Map.Field.iField getField(int X, int Y)
         {
-            // Przerobic przeliczanie sasiadujacych pol
-            if (X < 0) X = width;
-            if (Y < 0) Y = height;
-            // 2  > (2)
-            if (X >= width) X = 0;
-            if (Y >= height) Y = 0;
+            if (X < 0)
+            {
+                X = 0;
+            }
+            if (Y < 0)
+            {
+                Y = 0;
+            }
 
-            return this.fieldList[X,Y];
+            if (X >= width)
+            {
+                X = width-1;
+            }
+            if (Y >= height)
+            {
+                Y = height-1;
+            }
+            return this.fieldList[X, Y];
         }
 
-        public void LoadContent( ContentManager Content )
+        public void LoadContent(ContentManager Content)
         {
             for (int h = 0; h < this.height; h++)
             {
@@ -174,53 +185,6 @@ namespace World
                     this.fieldList[w, h].LoadContent(Content);
                 }
             }
-        }
-
-        public void Draw(SpriteBatch spriteBatch, Vector2 vCamera, Rectangle Viewport )
-        {
-            return;
-            // for (int h = 0; h < this.height; h++)
-            for (int h = 0; h < this.height; h++)
-            {
-                for (int w = 0; w < this.width; w++)
-                //for (int w = this.width-1; w >=0; w--)
-                {
-                    //iField
-                    World.Map.Field.iField field = this.fieldList[w, 0];//.Draw(spriteBatch, vCamera);
-/*                    field.Draw(spriteBatch, vCamera);
-                    field = field.adjacentFieldList[World.Map.Field.iField.E];
-                    field.Draw(spriteBatch, vCamera);
-                    field = field.adjacentFieldList[World.Map.Field.iField.E];
-                    field.Draw(spriteBatch, vCamera);
-                    field = field.adjacentFieldList[World.Map.Field.iField.E];
-                    field.Draw(spriteBatch, vCamera);
-                    field = field.adjacentFieldList[World.Map.Field.iField.E];
-                    field.Draw(spriteBatch, vCamera);
-                    field = field.adjacentFieldList[World.Map.Field.iField.E];
-                    field.Draw(spriteBatch, vCamera);
-*/                }
-            }
-        }
-
-        public string toString()
-        {
-            string buffor = "WxH " + this.width + "x" + this.height + "\n";
-            buffor += "    ";
-
-            for (int w = 0; w < this.width; w++)
-            {
-                buffor += w.ToString("000") + " ";
-            }
-
-            for (int h = 0; h < this.height; h++)
-            {
-                buffor += "\n" + h.ToString("000") + "  ";
-                for (int w = 0; w < this.width; w++)
-                {
-                    buffor += this.fieldList[w, h].ToString() + "   ";
-                }
-            }
-            return buffor; ;
         }
     }
 }
